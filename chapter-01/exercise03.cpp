f2fc7829f015d59f81708603c561b38262a1174d
Davide Fossacecchi <rocker.rabbit@gmail.com>
2015-03-10 22:54:08 +0100

    Get images from the camera and put them in a video file output

--- chapter-01/exercise03.cpp
+++ chapter-01/exercise03.cpp
@@ -1,3 +1,6 @@
+/*Author: Davide Fossacecchi
+ *Exercise 3*/
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -7,10 +10,16 @@ int main(int argc, char** argv){
 	namedWindow("Video", WINDOW_AUTOSIZE);
 	VideoCapture cap;
 	cap.open(0);
-	Mat frame;
+	Mat frame, resized;
+	cap >> frame;
+	pyrDown(frame, resized);
+	VideoWriter writer;
+	writer.open("output/exercise3.avi",CV_FOURCC('M','J','P','G'), 30, resized.size());
 	while(true){
 		cap >> frame;
-		imshow("Video",frame);
+		pyrDown(frame, resized);
+		//imshow("Video",resized);
+		writer << resized;
 		if(waitKey(33) >= 0) break;
 	}
 }
