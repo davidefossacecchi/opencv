c04c8d98c5f5798940137f3a817de370e990ceb6
Davide Fossacecchi <rocker.rabbit@gmail.com>
2015-03-15 23:56:11 +0100

    Clones the green channel twice

--- chapter-02/exercise06.cpp
+++ chapter-02/exercise06.cpp
@@ -3,16 +3,26 @@
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv){
-	Mat img = imread("rgb.png");
+	Mat img = imread("lena.bmp");
 	imshow("Original", img);
 	Mat channels[3], result;
 	split(img, channels);
 	imshow("Blue", channels[0]);
+	
+	//point a
 	imshow("Green", channels[1]);
 	imshow("Red", channels[2]);
+	
+	//point b
+	Mat clone1 = channels[1].clone();
+	Mat clone2 = channels[1].clone();
+	
+	
 	waitKey(0);
 }
